2d453986e8f3b26f1188608621973ed0e35e1fae
loannguyen90 <nguyenloan13690@gmail.com>
2023-12-13 15:41:53 +0900

    updste case study

--- spotify/src/components/DetailSong.js
+++ spotify/src/components/DetailSong.js
@@ -9,10 +9,10 @@ export default function DetailSong() {
             <h2 className='text-cyan-500 font-bold'>Now playing</h2>
             <h2 className='text-neutral-400 text-2xl'>{song.name}</h2>
             <div className="w-[240px] m-auto mt-10">
-                <img className='w-full' src={song.link.images[0].url} alt="avatar" />
+                <img className='w-[100px] w-full' src={song.link.images[0].url} alt="avatar" />
             </div>
             <div className='flex justify-evenly items-center mt-10'>
-                <img className="w-[100px] rounded-circle" src={song.link.images[1].url} alt="avatar" />
+                <img className="w-[100px] h-[100px] rounded-full" src={song.link.images[1].url} alt="avatar" />
                 <span className='text-xl text-white'>{song.author}</span>
             </div>
 
